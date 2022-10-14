eab4d55b44f69ea720c73d5f3a4256cf976d1298
Victor Delgado <valexdelgado@gmail.com>
2022-10-14 09:22:09 -0700

    can now search for a term and print the titles in the response

--- ajax-giphy-party/app.js
+++ ajax-giphy-party/app.js
@@ -1,16 +1,12 @@
-const $term = $('.searchText')
+const $searchTerm = $('.searchText')
 
-$('#search').on('submit', function(evt){
+$('#search').on('submit', async function(evt){
     evt.preventDefault()
     
-    let term = $term.val()
-    
-    
-    $("form").on("submit", async function(evt) {
+
         evt.preventDefault();
       
-        let term = $searchInput.val();
-        $searchInput.val("");
+        let term = $searchTerm.val();
       
         const response = await axios.get("http://api.giphy.com/v1/gifs/search", {
           params: {
@@ -18,15 +14,8 @@ $('#search').on('submit', function(evt){
             api_key: "qHUL6XPx3AgHw7Rh0Ivj6sGK8lOq6fCX"
           }
         });
-        addGif(response.data);
+        for (let dataPoint of response.data.data){
+            console.log(dataPoint.title)
+        };
       });
-})
 
-
-async function showStuff(query){
-    const response = await axios.get(`https://api.giphy.com/v1/gifs/search?api_key=qHUL6XPx3AgHw7Rh0Ivj6sGK8lOq6fCX&q=${query}&limit=25&offset=0&rating=g&lang=en`)
-
-    for (dataPoint of response.data.data){
-        console.log(dataPoint.title)
-    }
-}
