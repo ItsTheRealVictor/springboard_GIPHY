d92b37f81eaaece88ef090e15b6dfb9bee983e38
Victor Delgado <valexdelgado@gmail.com>
2022-10-14 23:11:25 -0700

    assignment finished, all requirements met

--- ajax-giphy-party/app.js
+++ ajax-giphy-party/app.js
@@ -1,17 +1,13 @@
 const $searchTerm = $('.searchText')
 const $remove = $('.removeButt')
 
-$remove.on('click', function(){
-    // clears all gifs out of the gifHolder container div
-    $('.gifHolder').empty()
-})
 
 $('#search').on('submit', async function(evt){
     evt.preventDefault()
     
-      
+    
+    
     let term = $searchTerm.val();
-      
     const response = await axios.get("http://api.giphy.com/v1/gifs/search", {
         params: {
         q: term,
@@ -22,20 +18,34 @@ $('#search').on('submit', async function(evt){
     //for debugging purposes
     // console.log('# of gifs: ' + response.data.data.length)
     // for (let dataPoint of response.data.data){
-    //     console.log(dataPoint)
-    // };
-    
-    
-    addGif()
+        //     console.log(dataPoint)
+        // };
+        
+        
+        addGif()
         function addGif() {
+            
             // the response gives you 50 gifs, we'll take a random index between 0 and 50
             let randomIndex = Math.floor(Math.random() * 50)
-            console.log(randomIndex)
+            
+            // for debugging
+            console.log(randomIndex) 
+            
+            // grab a random gif
             const gifURL = response.data.data[randomIndex].images.original.url
+            
+            // wrap it in a div, add a gifElement class in case we want to mess with its CSS later on
             const imgDiv = document.createElement('div')
             imgDiv.classList.add('gifElm')
+            
+            // append it to the larger gifHolder container 
             $('.gifHolder').append(imgDiv)
             $(imgDiv).append($('<img/>', {src:gifURL})) //
-    }
-});
-
+        }
+    });
+    
+    $remove.on('click', function(){
+        // clears all gifs out of the gifHolder container div
+        $('.gifHolder').empty()
+    })
+    
